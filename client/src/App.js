f599f1175edcee3840acb842e0159b840578f5af
Jamie S <62821565+teopatl@users.noreply.github.com>
2024-06-19 11:18:32 -0400

    Changed title per lecture

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React OR DIE
+            Fib calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
